36337ae2b4c07c1f1bc00e12c5752e52b2b790b7
nurillahilwafi <nurilwafi17@gmail.com>
2021-10-14 05:28:46 +0700

    Create MainActivity.java

---
+++ bottom_navigation/app/src/main/java/com/example/waappii/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.waappii;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomnavigation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomnavigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new chatFragment()).commit();
+
+        bottomnavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_chat:
+                        selectedFragment = new chatFragment();
+                        break;
+                    case R.id.nav_status:
+                        selectedFragment = new statusFragment();
+                        break;
+                    case R.id.nav_calls:
+                        selectedFragment = new callsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
